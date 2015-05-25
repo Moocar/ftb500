6fc5e13196a57f72ccbe975ed4a58c779afda8f3
Anthony Marcar <amarcar@Anthonys-MacBook-Air.local>
2015-05-24 17:09:51 -0700

    handle welcome user

--- ftb500/src/me/moocar/ftb500/client/app.cljs
+++ ftb500/src/me/moocar/ftb500/client/app.cljs
@@ -4,7 +4,7 @@
             [cljs-uuid-utils.core :refer [make-random-squuid]]
             [me.moocar.ftb500.client.websocket :as websocket]
             [me.moocar.remote :as remote])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (defn atom-input [value]
   [:input {:type "text"
@@ -18,30 +18,49 @@
       (remote/post client
                    {:route :user/add
                     :keys {:user/id (make-random-squuid)
-                           :user/name @val
-                           }}
+                           :user/name @val}}
                    response-ch)
       (.log js/console "sent to server")
       (go
         (.log js/console (str (<! response-ch)))))))
 
+(defn render-lobby [{:keys [db] :as client}]
+  [:h2 (str "Welcome " (:user/name (:user @db)))])
+
 (defn current-page [client]
-  (let [val (atom "enter here")]
-    [:div
-     [:h2 "What is your name?"]
-     [atom-input val]
-     [:input {:type "button"
-              :value "Set name"
-              :on-click (set-name-click client val)}]]))
+  (if-let [user (get-in @(:db client) [:user])]
+    (render-lobby client)
+    (let [name-field (atom "enter here")]
+      [:div
+       [:h2 "What is your name?"]
+       [atom-input name-field]
+       [:input {:type "button"
+                :value "Set name"
+                :on-click (set-name-click client val)}]])))
 
 (defn mount-root [client]
   (reagent/render [current-page client] (.getElementById js/document "app")))
 
 (defn start-client
-  [this]
-  (let [{:keys [server-chans]} this
-        mult (async/mult (:recv-ch server-chans))]
-    (assoc this :mult mult)))
+  [client]
+  (let [{:keys [server-chans route-app]} client
+        mult (async/mult (:recv-ch server-chans))
+        route-tap (async/chan)
+        route-pub (async/pub route-tap (comp :route :request))]
+    (async/tap mult route-tap)
+    (assoc client :mult mult :route-tap route-tap :route-pub route-pub)))
+
+(defn on-user-add [client]
+  (let [{:keys [route-pub db]} client
+        ch (async/chan)]
+    (async/sub route-pub :user/add ch)
+    (go-loop []
+      (when-let [user (:keys (:request (<! ch)))]
+        (swap! db assoc :user user)
+        (recur)))))
+
+(defn start-handlers [client]
+  (on-user-add client))
 
 (defn start []
   (.log js/console "mounted")
@@ -49,12 +68,14 @@
                          :recv-ch (async/chan)}
         log-ch (async/chan)
         client {:server-chans transport-chans
-                :log-ch log-ch}
+                :log-ch log-ch
+                :db (atom {})}
         client (start-client client)
         ws {:uri "ws://localhost:8080/ws"
             :transport-chans transport-chans
             :log-ch log-ch}
         ws (websocket/start ws)]
+    (start-handlers client)
     (go
       (loop []
         (when-let [msg (<! log-ch)]
